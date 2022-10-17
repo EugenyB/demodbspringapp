e072643d80056c761589bd4b9d11aba24e34b4e3
eugenyb <eberkunsky@gmail.com>
2022-10-17 19:24:59 +0300

    Added new functions

--- src/main/java/ua/mk/berkut/demodbspringapp/controller/DBController.java
+++ src/main/java/ua/mk/berkut/demodbspringapp/controller/DBController.java
@@ -8,15 +8,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ua.mk.berkut.demodbspringapp.entities.Gruppa;
+import ua.mk.berkut.demodbspringapp.entities.Student;
 import ua.mk.berkut.demodbspringapp.repository.GruppaRepository;
+import ua.mk.berkut.demodbspringapp.repository.StudentRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
 public class DBController {
 
     private GruppaRepository gruppaRepository;
+
+    private StudentRepository studentRepository;
 
     @GetMapping("/groups")
     public String showAllGroups(Model model) {
@@ -39,5 +44,50 @@ public class DBController {
         gruppaRepository.deleteById(id);
 
         return "redirect:/groups";
+    }
+
+    @GetMapping("/edit_group/{id}")
+    public String showUpdateGroupPage(@PathVariable("id") int id, Model model) {
+        Optional<Gruppa> gruppa = gruppaRepository.findById(id);
+        if (gruppa.isEmpty()) {
+            model.addAttribute("message", "Группы с id " + id + " нет");
+            return "error";
+        } else {
+            model.addAttribute("gr", gruppa.get());
+            return "edit_group";
+        }
+    }
+
+    @PostMapping("/update_group/{id}")
+    public String updateGroup(@PathVariable("id") int id, Gruppa gruppa) {
+        Optional<Gruppa> g1 = gruppaRepository.findById(id);
+        Gruppa gr = g1.get();
+        gr.setTitle(gruppa.getTitle());
+        gruppaRepository.save(gr);
+        return "redirect:/groups";
+    }
+
+    @GetMapping("/students_group/{id}")
+    public String viewStudentsByGroup(@PathVariable("id") int id, Model model) {
+        Optional<Gruppa> gruppa = gruppaRepository.findById(id);
+        if (gruppa.isEmpty()) {
+            model.addAttribute("message", "Группы с id " + id + " нет");
+            return "error";
+        } else {
+            model.addAttribute("gr", gruppa.get());
+            return "student_by_group";
+        }
+    }
+
+    @PostMapping("/find_student")
+    public String findStudentByName(@RequestParam String name, Model model) {
+        Optional<Student> student = studentRepository.findByName(name);
+        if(student.isEmpty()) {
+            model.addAttribute("message", "Студент с именем " + name + " не найден");
+            return "error";
+        } else {
+            model.addAttribute("student", student.get());
+            return "view_student";
+        }
     }
 }
--- src/main/java/ua/mk/berkut/demodbspringapp/repository/StudentRepository.java
+++ src/main/java/ua/mk/berkut/demodbspringapp/repository/StudentRepository.java
@@ -3,5 +3,9 @@ package ua.mk.berkut.demodbspringapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.mk.berkut.demodbspringapp.entities.Student;
 
+import java.util.Optional;
+
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+    Optional<Student> findByName(String name);
+
 }
